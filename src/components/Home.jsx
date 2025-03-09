64b2429597477dae4f0a7d72072802876c0736c1
Jacqueline Ohayon <jacquelineohayon@Jacquelines-MacBook-Pro.local>
2025-03-09 19:44:12 -0400

    Fixed Peepo image import

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import peepoImage from '../assets/images/peepo_bio.webp';
 
 function Home() {
   return (
@@ -10,7 +11,7 @@ function Home() {
           First of all: This is <mark><b>Peepo</b></mark>. I <em>love</em> peepo! 
           You'll see a lot of Peepo on this website, and he is also happy to see you here, too.
         </p>
-        <img src="/src/assets/images/peepo_bio.webp" alt="Peepo" width="280" height="200" />
+        <img src={peepoImage} alt="Peepo" width="280" height="200" />
         <blockquote>
           <i>"Jacqueline is a French name that means "supplanter". It's the feminine version of the name Jacques, which comes from the Hebrew name Jacob."</i>
           <p>My favorite color is <del>red</del> <ins>purple</ins>.</p>
