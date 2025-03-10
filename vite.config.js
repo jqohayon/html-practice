2b7533a152176da3aa48e0b3f2621988e91d7c72
jqohayon <jqohayon@gmail.com>
2025-03-09 21:22:53 -0400

    Simplify deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/html-practice/' : '/',
+  base: '',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
   }
 })
