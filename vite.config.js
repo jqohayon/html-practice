7d3a5071e0d094b43a61c0a6862361b4c3710125
jqohayon <jqohayon@gmail.com>
2025-03-09 21:58:33 -0400

    Fix GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/html-practice/',
+  base: '/html-practice/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
   }
-}))
+})
