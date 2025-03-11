5cc5ee9a8b6e9f1e7f501f31ddd32e009c74c1f8
jqohayon <jqohayon@gmail.com>
2025-03-10 23:57:25 -0400

    renaming convention jacqstack

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/html-practice/',
+  base: '/jacqstack/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
