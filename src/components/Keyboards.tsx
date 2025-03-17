7251a33b2910a0c8e76defe24872180e02a279bc
jqohayon <jqohayon@gmail.com>
2025-03-17 17:24:47 -0400

    Remove extra keyboard columns

--- src/components/Keyboards.tsx
+++ src/components/Keyboards.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 // Define the keyboard data structure
 interface Keyboard {
@@ -13,7 +13,7 @@ interface Keyboard {
   rgb: string;
 }
 
-const SAMPLE_KEYBOARDS: Keyboard[] = [
+const KEYBOARDS: Keyboard[] = [
   { id: 1, make: 'Cerakey', model: 'Nadapanda65', color: 'Green/Black/White', switch1: 'Linear', switch2: 'Celeste Blue', size: '65', link: 'https://www.cerakey.com/products/nada-65-panda-keyboard?srsltid=AfmBOorlYYZtQQaFo5_nPrSgZwU0B7QA40FvzBw3JzqMcCGCj_Lq_Ksc', rgb: 'Yes' },
   { id: 2, make: 'Cerakey', model: 'Nadapanda65', color: 'Red/White Heart', switch1: 'Linear', switch2: 'Celeste Blue', size: '65', link: '', rgb: 'Yes' },
   { id: 3, make: 'HHKB', model: 'Classic', color: 'Grey/Beige', switch1: 'Electrostatic', switch2: 'Topre', size: '60', link: '', rgb: 'No' },
@@ -24,34 +24,10 @@ const SAMPLE_KEYBOARDS: Keyboard[] = [
   { id: 8, make: 'Melgeek', model: 'Made68 Pro', color: 'Pink', switch1: 'Linear', switch2: 'Magnetic', size: '68', link: '', rgb: 'Yes' },
   { id: 9, make: 'Vortex', model: 'Hellcat', color: 'Purple', switch1: 'Linear', switch2: 'G Pro Silver', size: '65', link: '', rgb: 'Yes' },
   { id: 10, make: 'Vortex', model: 'Pok3r 2', color: 'Black', switch1: 'Linear/Tactile', switch2: 'MX Blue', size: '60', link: '', rgb: 'Yes' },
-  { id: 11, make: 'Vortex', model: 'Pok3r 3', color: 'White', switch1: 'Linear/Tactile', switch2: 'MX Red', size: '60', link: '', rgb: 'Yes' },
-  { id: 12, make: '', model: '', color: '', switch1: '', switch2: '', size: '', link: '', rgb: '' }
+  { id: 11, make: 'Vortex', model: 'Pok3r 3', color: 'White', switch1: 'Linear/Tactile', switch2: 'MX Red', size: '60', link: '', rgb: 'Yes' }
 ];
 
 const Keyboards: React.FC = () => {
-  // Load from localStorage or use sample data
-  const [keyboards, setKeyboards] = useState<Keyboard[]>(() => {
-    const savedKeyboards = localStorage.getItem('keyboards');
-    return savedKeyboards ? JSON.parse(savedKeyboards) : SAMPLE_KEYBOARDS;
-  });
-
-  // Save to localStorage whenever keyboards change
-  useEffect(() => {
-    localStorage.setItem('keyboards', JSON.stringify(keyboards));
-  }, [keyboards]);
-
-  // Function to handle cell edit
-  const handleCellChange = (id: number, field: keyof Keyboard, value: string) => {
-    setKeyboards(keyboards.map(keyboard => 
-      keyboard.id === id ? { ...keyboard, [field]: value } : keyboard
-    ));
-  };
-
-  // Function to reset to sample data
-  const resetToSampleData = () => {
-    setKeyboards(SAMPLE_KEYBOARDS);
-  };
-
   return (
     <div className="keyboards-container">
       <h1>Keyboards Collection</h1>
@@ -59,7 +35,6 @@ const Keyboards: React.FC = () => {
         <table className="keyboards-table">
           <thead>
             <tr>
-              <th>#</th>
               <th>Make</th>
               <th>Model</th>
               <th>Color</th>
@@ -71,78 +46,29 @@ const Keyboards: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {keyboards.map((keyboard) => (
+            {KEYBOARDS.map((keyboard) => (
               <tr key={keyboard.id}>
-                <td>{keyboard.id}</td>
-                <td>
-                  <input 
-                    type="text" 
-                    value={keyboard.make} 
-                    onChange={(e) => handleCellChange(keyboard.id, 'make', e.target.value)}
-                  />
-                </td>
-                <td>
-                  <input 
-                    type="text" 
-                    value={keyboard.model} 
-                    onChange={(e) => handleCellChange(keyboard.id, 'model', e.target.value)}
-                  />
-                </td>
-                <td>
-                  <input 
-                    type="text" 
-                    value={keyboard.color} 
-                    onChange={(e) => handleCellChange(keyboard.id, 'color', e.target.value)}
-                  />
-                </td>
-                <td>
-                  <input 
-                    type="text" 
-                    value={keyboard.switch1} 
-                    onChange={(e) => handleCellChange(keyboard.id, 'switch1', e.target.value)}
-                  />
-                </td>
-                <td>
-                  <input 
-                    type="text" 
-                    value={keyboard.switch2} 
-                    onChange={(e) => handleCellChange(keyboard.id, 'switch2', e.target.value)}
-                  />
-                </td>
-                <td>
-                  <input 
-                    type="text" 
-                    value={keyboard.size} 
-                    onChange={(e) => handleCellChange(keyboard.id, 'size', e.target.value)}
-                  />
-                </td>
+                <td>{keyboard.make}</td>
+                <td>{keyboard.model}</td>
+                <td>{keyboard.color}</td>
+                <td>{keyboard.switch1}</td>
+                <td>{keyboard.switch2}</td>
+                <td>{keyboard.size}</td>
                 <td className="link-cell">
-                  <input 
-                    type="text" 
-                    value={keyboard.link} 
-                    onChange={(e) => handleCellChange(keyboard.id, 'link', e.target.value)}
-                  />
-                  {keyboard.link && (
+                  {keyboard.link ? (
                     <a href={keyboard.link} target="_blank" rel="noopener noreferrer" className="visit-link">
                       Visit
                     </a>
+                  ) : (
+                    "N/A"
                   )}
                 </td>
-                <td>
-                  <input 
-                    type="text" 
-                    value={keyboard.rgb} 
-                    onChange={(e) => handleCellChange(keyboard.id, 'rgb', e.target.value)}
-                  />
-                </td>
+                <td>{keyboard.rgb}</td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <button className="reset-button" onClick={resetToSampleData}>
-        Reset to Default Data
-      </button>
     </div>
   );
 };
