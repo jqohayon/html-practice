88a1322b2d6b81cffd2d52d63737e2f3eeff14dc
jqohayon <jqohayon@gmail.com>
2025-03-17 20:52:41 -0400

    github router config

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
